a4d92e03508e3844badad8041ede1516f0660222
David Gavilan <david.gavilan.ruiz@gmail.com>
2015-02-16 23:43:42 +0000

    Refactor the draw functions in SH.js so they can be reused for different canvas at the same time.

--- SH/sh.js
+++ SH/sh.js
@@ -6,10 +6,6 @@ function Resources(gl, width, height)
 	this.gl = gl;
 	this.width = width;
 	this.height = height;
-	this.rb = null; // render buffer
-	this.fb = null; // frame buffer
-	this.txOITAccumulation = null; // accumulation texture for OIT
-	this.txOITReveal = null; // reveal texture for OIT
 	// state
 	this.oldFBO = 0;
 	this.oldRBO = 0;
@@ -23,43 +19,17 @@ function Resources(gl, width, height)
 	// WebGL extensions
 	this.extFloat = null;
 	this.initExtensions();
-	this.initBuffers();
-	this.initShaders();
-	this.initGeometry();
 }
 
 Resources.prototype.initExtensions = function()
 {
+	// @todo compute Spherical Harmonics in the GPU using float textures
 	this.extFloat = this.gl.getExtension('OES_texture_float');
 	if (!this.extFloat) {
 		alert("Missing OES_texture_float extension!");
 		return false;
 	}
 }
-
-Resources.prototype.initShaders = function()
-{
-	var gl = this.gl;
-	this.shader["color"] = GFX.useShader(gl, "shaders/geometryColor.vs", "shaders/color.fs");
-	
-	// vertex attributes
-	this.attrib["color"] = {}
-	this.attrib["color"].color = gl.getAttribLocation(this.shader["color"], "color");
-	this.attrib["color"].position = gl.getAttribLocation(this.shader["color"], "position");
-	// uniforms
-	this.uniform["color"] = {}
-	this.uniform["color"].Pmatrix = gl.getUniformLocation(this.shader["color"], "Pmatrix");
-	this.uniform["color"].Vmatrix = gl.getUniformLocation(this.shader["color"], "Vmatrix");
-	this.uniform["color"].Mmatrix = gl.getUniformLocation(this.shader["color"], "Mmatrix");
-	gl.enableVertexAttribArray(this.attrib["color"].color);
-	gl.enableVertexAttribArray(this.attrib["color"].position);
-}
-
-Resources.prototype.initBuffers = function()
-{
-
-}
-
 
 Resources.prototype.setDefaultTextureParameters = function()
 {
@@ -68,54 +38,6 @@ Resources.prototype.setDefaultTextureParameters = function()
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S,     gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T,     gl.CLAMP_TO_EDGE);
-}
-
-Resources.prototype.initGeometry = function()
-{
-	var gl = this.gl;
-	// geometry
-	this.quad = [ // xyz, uv
-		-1, -1, 0,  0, 0,
-		-1,  1, 0,  0, 1,
-		 1, -1, 0,  1, 0,
-		 1,  1, 0,  1, 1
-	];
-	this.quadFaces = [0, 2, 1, 3];
-	this.quadVB = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, this.quadVB);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.quad), gl.STATIC_DRAW);
-	this.quadIB = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.quadIB);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.quadFaces), gl.STATIC_DRAW);
-	
-	// dots	
-	this.buffer["dots"] = gl.createBuffer();
-    this.buffer["dotsColor"] = gl.createBuffer();
-	var vertices = [
-         0.0,  1.0,  0.0,
-        -1.0, -1.0,  0.0,
-         1.0, -1.0,  0.0
-    ];
-    var colors = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0
-    ];
-    this.setDots(vertices, colors);
-}
-
-// dots for debugging samples
-Resources.prototype.setDots = function(vertices, colors)
-{
-	var gl = this.gl;
-	gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer["dots"]);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    this.buffer["dots"].itemSize = 3;
-    this.buffer["dots"].numItems = vertices.length / 3;
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer["dotsColor"]);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    this.buffer["dotsColor"].itemSize = 4;
-    this.buffer["dotsColor"].numItems = colors.length / 4;	
 }
 
 // ----------------------------------------------
@@ -146,118 +68,180 @@ Resources.prototype.popViewport = function()
 	var v = this.oldViewport;
 	this.gl.viewport(v[0], v[1], v[2], v[3]);
 }
-// ----------------------------------------------
-// Drawing ops
-// ----------------------------------------------
-Resources.prototype.drawFullScreenQuad = function()
-{
-	var gl = this.gl;
-	gl.bindBuffer(gl.ARRAY_BUFFER, this.quadVB);
-	gl.vertexAttribPointer(this.attribPosition, 3, gl.FLOAT, false, 4*(3+2), 0);
-	gl.vertexAttribPointer(this.attribUv, 2, gl.FLOAT, false, 4*(3+2), 4*3);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.quadIB);
-	gl.drawElements(gl.TRIANGLE_STRIP, this.quadFaces.length, gl.UNSIGNED_SHORT, 0);
-}
+
 
 // ============================================
-var main = function(vertices, colors)
+function CanvasRenderer(canvasId)
 {
-	var canvas = document.getElementById("debugSamplesCanvas");
-	// -------------------------------------------------
-	// capture mouse events
-	// -------------------------------------------------
-	var amortization=0.95;
-	var drag=false;
-	var old_x, old_y;
-	var dX=0, dY=0;
-	
-	var mouseDown=function(e) {
-	  drag=true;
-	  old_x=e.pageX, old_y=e.pageY;
+	var self = this;
+	this.canvas = document.getElementById(canvasId);
+	this.mouseState = {
+		amortization: 0.95,
+		drag: false,
+		old_x: 0,
+		old_y: 0,
+		dX: 0,
+		dY: 0,
+		theta: 0,
+		phi: 0,
+	};
+	this.mouseDown = function(e) {
+		self.mouseState.drag=true;
+		self.mouseState.old_x=e.pageX;
+		self.mouseState.old_y=e.pageY;
+		e.preventDefault();
+		return false;
+	}
+	this.mouseUp = function(e) {
+		self.mouseState.drag=false;
+	};
+	this.mouseMove = function(e) {
+	  if (!self.mouseState.drag) 
+	  	return false;
+	  self.mouseState.dX=(e.pageX-self.mouseState.old_x)*Math.PI/self.canvas.width;
+	  self.mouseState.dY=(e.pageY-self.mouseState.old_y)*Math.PI/self.canvas.height;
+	  self.mouseState.theta += self.mouseState.dX;
+	  self.mouseState.phi += self.mouseState.dY;
+	  self.mouseState.old_x=e.pageX;
+	  self.mouseState.old_y=e.pageY;
 	  e.preventDefault();
-	  return false;
 	};
+	// matrices
+	this.projectionMatrix = MATH.getProjection(40, this.canvas.width/this.canvas.height, 1, 100);
+	this.modelMatrix = MATH.getI4();
+	this.viewMatrix = MATH.getI4();
+
 	
-	var mouseUp=function(e){
-	  drag=false;
-	};
+	// Overwrite this function with your custom stuff
+	this.drawModel = function() {	
+	}
 	
-	var mouseMove=function(e) {
-	  if (!drag) return false;
-	  dX=(e.pageX-old_x)*Math.PI/canvas.width,
-	    dY=(e.pageY-old_y)*Math.PI/canvas.height;
-	  THETA+=dX;
-	  PHI+=dY;
-	  old_x=e.pageX, old_y=e.pageY;
-	  e.preventDefault();
-	};
+	this.resources = {};
 	
-	canvas.addEventListener("mousedown", mouseDown, false);
-	canvas.addEventListener("mouseup", mouseUp, false);
-	canvas.addEventListener("mouseout", mouseUp, false);
-	canvas.addEventListener("mousemove", mouseMove, false);
-  
-	// -------------------------------------------------
+	if (this.init()) 
+	{
+		this.start = function() {
+			var gl = self.resources.gl;
+			var res = self.resources;
+			MATH.translateZ(self.viewMatrix, -2);
+						
+			// Drawing
+			gl.clearColor(0,0,0,1);
+			gl.enable(gl.DEPTH_TEST);
+			gl.depthFunc(gl.LEQUAL);
+			gl.clearDepth(1.0);
+			var time_old=0;
+			var animate = function(time)
+			{
+				var dt=time-time_old;
+				if (!self.mouseState.drag) {
+				  self.mouseState.dX*=self.mouseState.amortization;
+				  self.mouseState.dY*=self.mouseState.amortization;
+				  self.mouseState.theta+=self.mouseState.dX;
+				  self.mouseState.phi+=self.mouseState.dY;
+				}
+				MATH.setI4(self.modelMatrix);
+				MATH.rotateY(self.modelMatrix, self.mouseState.theta);
+				MATH.rotateX(self.modelMatrix, self.mouseState.phi);
+				time_old=time;
+			
+				gl.viewport(0, 0, self.canvas.width, self.canvas.height);
+				gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+				
+				// should be overwritten
+				self.drawModel();
+				
+				gl.flush(); 
+				window.requestAnimationFrame(animate); // redraw the scene
+			}
+			
+			animate(0); // first lunch
+			
+		}
+	} else {
+		this.start = function() {
+			console.log("Failed to initialize canvas.");
+		}
+	}
+}
+
+CanvasRenderer.prototype.init = function()
+{
+	// Register mouse event listeners
+	this.canvas.addEventListener("mousedown", this.mouseDown, false);
+	this.canvas.addEventListener("mouseup", this.mouseUp, false);
+	this.canvas.addEventListener("mouseout", this.mouseUp, false);
+	this.canvas.addEventListener("mousemove", this.mouseMove, false);  
 	// Get WebGL context
-	// -------------------------------------------------
 	var gl;
 	try {
-		gl = canvas.getContext("experimental-webgl", {antialias: true});
+		gl = this.canvas.getContext("experimental-webgl", {antialias: true});
 	} catch (e) {
 		alert("Your browser is not WebGL compatible :(");
 		return false;
 	}
-	// ------------------------------------
 	// Resources
-	// ------------------------------------
-	var res = new Resources(gl, canvas.width, canvas.height);
-	res.setDots(vertices, colors);
-	
-	// ------------------------------------
-	// matrices
-	// ------------------------------------
-	var projectionMatrix = MATH.getProjection(40, canvas.width/canvas.height, 1, 100);
-	var modelMatrix = MATH.getI4();
-	var viewMatrix = MATH.getI4();
-	MATH.translateZ(viewMatrix, -2);
-	var THETA=0,
-      	PHI=0;
-	
-	
-	// Drawing
-	gl.clearColor(0,0,0,1);
-	gl.enable(gl.DEPTH_TEST);
-	gl.depthFunc(gl.LEQUAL);
-	gl.clearDepth(1.0);
-	var time_old=0;
-	var animate = function(time)
-	{
-		var dt=time-time_old;
-		if (!drag) {
-		  dX*=amortization, dY*=amortization;
-		  THETA+=dX, PHI+=dY;
-		}
-		MATH.setI4(modelMatrix);
-		MATH.rotateY(modelMatrix, THETA);
-		MATH.rotateX(modelMatrix, PHI);
-		time_old=time;
-    
-		gl.viewport(0, 0, canvas.width, canvas.height);
-		gl.clearColor(0.1,0.1,0,1);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		
+	this.resources = new Resources(gl, this.canvas.width, this.canvas.height);
+	return true;
+}
+
+// ============================================
+/// To draw colored points
+function DotRenderer(canvasId)
+{
+	var self = this;
+	this.renderer = new CanvasRenderer(canvasId);
+	var res = self.renderer.resources;
+	var gl = res.gl;
+	// create shaders & attributes
+	this.initShaders();
+	// create vertex buffers
+	res.buffer["dots"] = gl.createBuffer();
+    res.buffer["dotsColor"] = gl.createBuffer();
+    // overwrite draw function
+	this.renderer.drawModel = function() {
 		gl.useProgram(res.shader["color"]);
-		gl.uniformMatrix4fv(res.uniform["color"].Pmatrix, false, projectionMatrix);
-		gl.uniformMatrix4fv(res.uniform["color"].Vmatrix, false, viewMatrix);
-		gl.uniformMatrix4fv(res.uniform["color"].Mmatrix, false, modelMatrix);
+		gl.uniformMatrix4fv(res.uniform["color"].Pmatrix, false, self.renderer.projectionMatrix);
+		gl.uniformMatrix4fv(res.uniform["color"].Vmatrix, false, self.renderer.viewMatrix);
+		gl.uniformMatrix4fv(res.uniform["color"].Mmatrix, false, self.renderer.modelMatrix);
 		gl.bindBuffer(gl.ARRAY_BUFFER, res.buffer["dots"]);
 		gl.vertexAttribPointer(res.attrib["color"].position, res.buffer["dots"].itemSize, gl.FLOAT, false, 0, 0);
 		gl.bindBuffer(gl.ARRAY_BUFFER, res.buffer["dotsColor"]);
 		gl.vertexAttribPointer(res.attrib["color"].color, res.buffer["dotsColor"].itemSize, gl.FLOAT, false, 0, 0);
 		gl.drawArrays(gl.POINTS, 0, res.buffer["dots"].numItems);  	
-		gl.flush(); 
-		window.requestAnimationFrame(animate); // redraw the scene
 	}
+}
+
+// dots for debugging samples
+DotRenderer.prototype.setDots = function(vertices, colors)
+{
+	var res = this.renderer.resources;
+	var gl = res.gl;
+	gl.bindBuffer(gl.ARRAY_BUFFER, res.buffer["dots"]);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    res.buffer["dots"].itemSize = 3;
+    res.buffer["dots"].numItems = vertices.length / 3;
+    gl.bindBuffer(gl.ARRAY_BUFFER, res.buffer["dotsColor"]);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    res.buffer["dotsColor"].itemSize = 4;
+    res.buffer["dotsColor"].numItems = colors.length / 4;	
+}
+
+DotRenderer.prototype.initShaders = function()
+{
+	var res = this.renderer.resources;
+	var gl = res.gl;
+	res.shader["color"] = GFX.useShader(gl, "shaders/geometryColor.vs", "shaders/color.fs");
 	
-	animate(0); // first lunch
+	// vertex attributes
+	res.attrib["color"] = {}
+	res.attrib["color"].color = gl.getAttribLocation(res.shader["color"], "color");
+	res.attrib["color"].position = gl.getAttribLocation(res.shader["color"], "position");
+	// uniforms
+	res.uniform["color"] = {}
+	res.uniform["color"].Pmatrix = gl.getUniformLocation(res.shader["color"], "Pmatrix");
+	res.uniform["color"].Vmatrix = gl.getUniformLocation(res.shader["color"], "Vmatrix");
+	res.uniform["color"].Mmatrix = gl.getUniformLocation(res.shader["color"], "Mmatrix");
+	gl.enableVertexAttribArray(res.attrib["color"].color);
+	gl.enableVertexAttribArray(res.attrib["color"].position);
 }
